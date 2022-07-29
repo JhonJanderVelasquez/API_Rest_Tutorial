99e40411731992f054af0555a75a943125b38408
JhonJanderVelasquez <jhonspeinx@gmail.com>
2022-07-28 21:23:35 -0500

    Creación de models, views.py con get, urls y admin

--- companyAPIProject/companyAPI/views.py
+++ companyAPIProject/companyAPI/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import View
+from .models import Company
+from django.http import JsonResponse
 
 # Create your views here.
+class CompanyListView (View):
+    def get (self, request):
+        list = Company.objects.all()
+        return JsonResponse(list, False)
+
+    # def post (self, request):
+    #     pass
+    # def put (self, request):
+    #     pass
+    # def delete (self, request):
+    #     pass
+
+class CompanyDetailView (View):
+    def get (self, request):
+        list = Company.objects.get(pk=pk)
+        return JsonResponse(list, False)
+
+    # def post (self, request):
+    #     pass
+    # def put (self, request):
+    #     pass
+    # def delete (self, request):
+    #     pass
+    
